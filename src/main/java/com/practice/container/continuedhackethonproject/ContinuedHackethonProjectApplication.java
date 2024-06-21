d5d76f24cad070b86911b14c0dc2cebcc39cae1a
takumi0706 <ganndamu0706@gmail.com>
2024-06-21 22:01:22 +0900

    feat:first commit

---
+++ src/main/java/com/practice/container/continuedhackethonproject/ContinuedHackethonProjectApplication.java
@@ -0,0 +1,13 @@
+package com.practice.container.continuedhackethonproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContinuedHackethonProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContinuedHackethonProjectApplication.class, args);
+    }
+
+}

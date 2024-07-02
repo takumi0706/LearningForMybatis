b565c17a22611163755d2fd93d20aeefc6ddd2cd
takumi <ganndamu0706@gmail.com>
2024-07-02 16:57:34 +0900

    feat:Change file component.Because difficult find where is their file.

---
+++ springboot/src/test/java/com/develop/TravelTango/TravelTangoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.develop.TravelTango;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelTangoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
